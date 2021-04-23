d5e517c30e6048e21a5e7cca2c16f712fcaf8c3b
Nishad Prinja <NishadPrinja@nishadprinjaserver.local>
2021-04-23 16:30:06 -0400

    adding all files worked on until search functionality

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chat_app/helper/authenticate.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff145C9E),
+        scaffoldBackgroundColor: Color(0xff1F1F1F),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Authenticate(),
+    );
+  }
+}
---
+++ lib/views/chatRoomsScreen.dart
@@ -0,0 +1,46 @@
+import 'package:chat_app/helper/authenticate.dart';
+import 'package:chat_app/services/auth.dart';
+import 'package:chat_app/views/search.dart';
+import 'package:flutter/material.dart';
+
+class ChatRoom extends StatefulWidget {
+  @override
+  _ChatRoomState createState() => _ChatRoomState();
+}
+
+class _ChatRoomState extends State<ChatRoom> {
+
+  AuthMethods authMethods = new AuthMethods();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset("assets/images/logo.png",
+        height:  50,),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              authMethods.signOut();
+              Navigator.pushReplacement(context, MaterialPageRoute(
+                  builder: (context) => Authenticate()
+              ));
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.exit_to_app)
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(
+              builder: (context) => SearchScreen()
+          ));
+        },
+      ),
+    );
+  }
+}
